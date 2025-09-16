3ebda66f7e66a8ce2134c248a295943e33cafab7
Sameer rai <sameerrai350@gmail.com>
2025-09-16 16:16:28 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.1 }}
             >
-              Hi, I'm <span className="text-primary">Sameer Rai</span>
+              Hi, I'm <span className="text-primary">Sameer</span>
             </motion.h1>
             
             <motion.p 
